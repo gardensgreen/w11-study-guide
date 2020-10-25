216ca3e69ad2dd7195ccdbbc36af9018a8b36ac9
Daniel TIllero <danieltillero67@gmail.com>
2020-10-25 13:41:19 -0400

    Express Setup and / route

--- express-practice-assessment/starter/app.js
+++ express-practice-assessment/starter/app.js
@@ -1,19 +1,36 @@
-/**
- * TODO: Create and configure your Express.js application in here.
- *       You must name the variable that contains your Express.js
- *       application "app" because that is what is exported at the
- *       bottom of the file.
- */
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+const { Person, HairColor } = require("./models");
 
+//setup route middlewares
+const csrfProtextion = csrf({ cookie: true });
 
+const app = express();
 
+//View Engine
+app.set("view engine", "pug");
 
+//Middleware
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
+//Routes
+app.get("/", async (req, res) => {
+    const people = await Person.findAll({
+        include: HairColor,
+    });
 
+    res.render("people-list", { people });
+});
+
+//Server
+const port = 8081;
+app.listen(port, () => console.log(`App is listening on port ${port}...`));
 
 /* Do not change this export. The tests depend on it. */
 try {
-  exports.app = app;
-} catch(e) {
-  exports.app = null;
+    exports.app = app;
+} catch (e) {
+    exports.app = null;
 }
